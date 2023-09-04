f886f8461ee6a24d7ec80cd1b09a1628d372103f
itsMont <jmontanaa@unal.edu.co>
2023-09-03 19:21:48 -0500

    enhanced game. Now decides the winner correctly

--- main.py
+++ main.py
@@ -8,6 +8,97 @@ def getNumPlayers():
         return 0
     return numPlayers
 
+# Turn it into a hidden phrase
+def createHiddenPhrase(phrase):
+    hiddenPhrase = ""
+    for i in phrase:
+        if i != ' ':
+            hiddenPhrase += "_"
+        else:
+            hiddenPhrase += i
+    return hiddenPhrase.split()
+
+# Receives a hiddenPhrase as a list and prints it
+def displayHiddenPhrase(hidden):
+    for i in hidden:
+        print(i, end = " ")
+    print("\n\n")
+
+# This makes a guess and returns the hidden phraese with the character guesses uncovered
+def makeGuess(guess, originalPhrase, hiddenPhrase):
+    # It doesnt matter if the guess is in lowercase or uppercase
+    lowerOriginalPhrase = originalPhrase.lower()
+    guess = guess.lower()
+
+    # keep a list with original characters
+    phraseList = originalPhrase.split()
+
+    if guess in lowerOriginalPhrase:
+        for i in range(len(phraseList)):
+            if guess in phraseList[i].lower():
+                newWord = ""
+                for j in range(len(phraseList[i])):
+                    if phraseList[i][j].lower() == guess:
+                        # Copies the exact letter according to original phrase
+                        newWord += phraseList[i][j]
+                    else:
+                        newWord += hiddenPhrase[i][j]
+                hiddenPhrase[i] = newWord
+    else:
+        print("'{}' It's not in the phrase".format(guess))
+    displayHiddenPhrase(hiddenPhrase)
+    return hiddenPhrase
+
+# Checks if players can still make a guess
+def playersStillGuess(players):
+    sum = 0
+    for i in players.values():
+        sum += i
+    if sum <= 0:
+        return False
+    return True
+# Check if a specified player can guess
+def playerCanGuess(players, player):
+    return players[player] > 0
+
+# decrease tries for player
+def decreaseTries(players, player):
+    players[player] -= 1
+
+# copies content from a list to another list
+def copyList(original):
+    newList = []
+    for i in original:
+        newList.append(i)
+    return newList
+
+# compares original list with guess list
+def comparesToGuess(originalList, guessList):
+    for i in range(len(originalList)):
+        if originalList[i] != guessList[i]:
+            return False
+    return True
+
+# function to receive the guess input correctly:
+def receiveGuess():
+    receive = True
+    while receive:
+        try:
+            guess = input()[0]
+            # In case user inputs a number or whatsoever
+            if len(guess) > 1:
+                raise RuntimeError
+            if not guess.isalpha():
+                raise ValueError
+            receive = False
+
+        except ValueError:
+            print("La esperanta frazo ne havas nombrojn / The esperanto phrase doesn't have a number")
+
+        except:
+            print("Faru validan divenon / Make a valid guess.")
+    return guess
+
 numPlayers = 0
 print("""
 
@@ -70,47 +161,6 @@ selPhrase = random.choice(phrases)
 print(selPhrase)
 phraseList = selPhrase.split()
 
-# Turn it into a hidden phrase
-def createHiddenPhrase(phrase):
-    hiddenPhrase = ""
-    for i in phrase:
-        if i != ' ':
-            hiddenPhrase += "_"
-        else:
-            hiddenPhrase += i
-    return hiddenPhrase.split()
-
-# Receives a hiddenPhrase as a list and prints it
-def displayHiddenPhrase(hidden):
-    for i in hidden:
-        print(i, end = " ")
-    print("\n\n")
-
-# This makes a guess and returns the hidden phraese with the character guesses uncovered
-def makeGuess(guess, originalPhrase, hiddenPhrase):
-    # It doesnt matter if the guess is in lowercase or uppercase
-    lowerOriginalPhrase = originalPhrase.lower()
-    guess = guess.lower()
-
-    # keep a list with original characters
-    phraseList = originalPhrase.split()
-
-    if guess in lowerOriginalPhrase:
-        for i in range(len(phraseList)):
-            if guess in phraseList[i].lower():
-                newWord = ""
-                for j in range(len(phraseList[i])):
-                    if phraseList[i][j].lower() == guess:
-                        # Copies the exact letter according to original phrase
-                        newWord += phraseList[i][j]
-                    else:
-                        newWord += hiddenPhrase[i][j]
-                hiddenPhrase[i] = newWord
-    else:
-        print("'{}' It's not in the phrase".format(guess))
-    displayHiddenPhrase(hiddenPhrase)
-    return hiddenPhrase
-
 hiddenList = createHiddenPhrase(selPhrase)
 
 print("""
@@ -121,35 +171,6 @@ This is the phrase you must guess:
 """)
 displayHiddenPhrase(hiddenList)
 
-# Checks if players can still make a guess
-def playersStillGuess(players):
-    sum = 0
-    for i in players.values():
-        sum += i
-    if sum <= 0:
-        return False
-    return True
-# Check if a specified player can guess
-def playerCanGuess(players, player):
-    return players[player] > 0
-
-# decrease tries for player
-def decreaseTries(players, player):
-    players[player] -= 1
-
-# copies content from a list to another list
-def copyList(original):
-    newList = []
-    for i in original:
-        newList.append(i)
-    return newList
-
-# compares original list with guess list
-def comparesToGuess(originalList, guessList):
-    for i in range(len(originalList)):
-        if originalList[i] != guessList[i]:
-            return False
-    return True
 # Ask the players to make a guess until they can't or the whole phrase is uncovered
 idPlayer = 0
 # Create an empty string to save the winner nickname
@@ -160,9 +181,9 @@ while(not comparesToGuess(phraseList, hiddenList) and playersStillGuess(players)
     originalTries = players[currentPlayer]
     winner = currentPlayer
     while playerCanGuess(players, currentPlayer):
-        print("Now guess {}".format(currentPlayer))
+        print("Divenu {current} / Now guess {current}".format(current = currentPlayer))
         # Receives the first character of the word it gets
-        guess = input()[0]
+        guess = receiveGuess()
         # Need to compare the original hideen List to the one after the player makesa a guess
         original = copyList(hiddenList)
         # Update the hiddenList with the guess of the player
@@ -173,6 +194,7 @@ while(not comparesToGuess(phraseList, hiddenList) and playersStillGuess(players)
             decreaseTries(players, currentPlayer)
             print(players)
             break
+        # In case the player guess the last chasracter to be guessed
         if comparesToGuess(phraseList, hiddenList):
             break
     idPlayer = (idPlayer + 1) % numPlayers
@@ -183,5 +205,5 @@ if not comparesToGuess(phraseList, hiddenList):
     print("La esperanta frazo estis: / The Esperanto Phrase was:")
     print(selPhrase)
 else:
-    print("Gratulon {}!, vi gajnis / Congrats {}!, you won".format(winner,winner))
+    print("Gratulon {}!, vi gajnis/ Congrats {}!, you won".format(winner,winner))
 
